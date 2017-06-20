fe67c198e843633bf579dd6c886dcee59191aeb8
heghin <a.heghine@gmail.com>
2017-06-20 10:06:51 +0400

    avelacri pythonum

---
+++ Heghine_Ayvazyan/Python/paskali erankyuni.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+minchev = 1+ input("Grel tiv@")
+sp=[]
+for i in range(0,minchev ):
+        sp.append([])
+i=1
+sp[0].append(1)
+for i in range(1,minchev ):
+    sp[i].append(1)
+    for j in range(1,i-1):
+            k=sp[i-1][j-1]+sp[i-1][j]
+            sp[i].append(k)
+    sp[i].append(1)
+sp.__delitem__(1)
+for i in sp:
+        print(i)
+input()
+
+exit ()
