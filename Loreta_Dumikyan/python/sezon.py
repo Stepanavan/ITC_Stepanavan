a49daddcdcb9a5ee12d4964204ca0b2b6117f9e2
loretadumikyan <loretttta@mail.ru>
2017-06-28 23:47:37 +0400

    sezon

---
+++ Loreta_Dumikyan/python/sezon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+x = int(input ("nermucel tiv: "))
+if x >= 3 and x<=5:
+	print ("garun")
+if x >= 6 and x<=8:
+	print ("amar")
+if x >= 9 and x<=11:
+	print ("ashun")
+if x == 12 or x == 1 or x == 2:
+	print ("dzmer")	
+else:
+	print ("nermucac tiv@ petq e lini 1-12 mijakayqum" )			
+
